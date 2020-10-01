967afc6d86126b421640131318247169dbd7eb5b
Andreas Risager <andreasbarkov@gmail.com>
2020-10-01 15:01:53 +0200

    s

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,3 @@
-// function showArtist(){
-//     fetch(`http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=rap&api_key=615b0297208e101fbc822a42b7acfe90&format=json`)
-//     .then(response => response.json())
-//     .then(function(data){
-//         //console.log(artists);
-//         let result = data.topartists.artist;
-        
-//         let container = document.querySelector(".glider-track");
-//         let template = document.querySelector("template");
-//         for(let i=0; i<5; i++){
-//             console.log(result[i]);
-
-//             let clone = template.content.cloneNode(true);
-//             let img = clone.querySelector("img");
-//             let name = clone.querySelector("h2");
-            
-    
-//             img.src = result[i].image[4]['#text'];
-//             name.innerText = result[i].name;
-            
-    
-//             container.appendChild(clone);
-//         }
-//         // result.forEach(function(artist){
-            
-//         // })
-//     })
-        
-// }
-// showArtist();
 !function(e){"function"==typeof define&&define.amd?define(e):"object"==typeof exports?module.exports=e():e()}(function(){var a="undefined"!=typeof window?window:this,e=a.Glider=function(e,t){var o=this;if(e._glider)return e._glider;if(o.ele=e,o.ele.classList.add("glider"),(o.ele._glider=o).opt=Object.assign({},{slidesToScroll:1,slidesToShow:1,resizeLock:!0,duration:.5,easing:function(e,t,o,i,r){return i*(t/=r)*t+o}},t),o.animate_id=o.page=o.slide=0,o.arrows={},o._opt=o.opt,o.opt.skipTrack)o.track=o.ele.children[0];else for(o.track=document.createElement("div"),o.ele.appendChild(o.track);1!==o.ele.children.length;)o.track.appendChild(o.ele.children[0]);o.track.classList.add("glider-track"),o.init(),o.resize=o.init.bind(o,!0),o.event(o.ele,"add",{scroll:o.updateControls.bind(o)}),o.event(a,"add",{resize:o.resize})},t=e.prototype;return t.init=function(e,t){var o=this,i=0,r=0;o.slides=o.track.children,[].forEach.call(o.slides,function(e,i){e.classList.add("glider-slide");e.setAttribute("data-gslide",i)}),o.containerWidth=o.ele.clientWidth;var s=o.settingsBreakpoint();if(t=t||s,"auto"===o.opt.slidesToShow||void 0!==o.opt._autoSlide){var l=o.containerWidth/o.opt.itemWidth;o.opt._autoSlide=o.opt.slidesToShow=o.opt.exactWidth?l:Math.floor(l)}"auto"===o.opt.slidesToScroll&&(o.opt.slidesToScroll=Math.floor(o.opt.slidesToShow)),o.itemWidth=o.opt.exactWidth?o.opt.itemWidth:o.containerWidth/o.opt.slidesToShow,[].forEach.call(o.slides,function(e){e.style.height="auto",e.style.width=o.itemWidth+"px",i+=o.itemWidth,r=Math.max(e.offsetHeight,r)}),o.track.style.width=i+"px",o.trackWidth=i,o.isDrag=!1,o.preventClick=!1,o.opt.resizeLock&&o.scrollTo(o.slide*o.itemWidth,0),(s||t)&&(o.bindArrows(),o.buildDots(),o.bindDrag()),o.updateControls(),o.emit(e?"refresh":"loaded")},t.bindDrag=function(){var t=this;t.mouse=t.mouse||t.handleMouse.bind(t);function e(){t.mouseDown=void 0,t.ele.classList.remove("drag"),t.isDrag&&(t.preventClick=!0),t.isDrag=!1}var o={mouseup:e,mouseleave:e,mousedown:function(e){e.preventDefault(),e.stopPropagation(),t.mouseDown=e.clientX,t.ele.classList.add("drag")},mousemove:t.mouse,click:function(e){t.preventClick&&(e.preventDefault(),e.stopPropagation()),t.preventClick=!1}};t.ele.classList.toggle("draggable",!0===t.opt.draggable),t.event(t.ele,"remove",o),t.opt.draggable&&t.event(t.ele,"add",o)},t.buildDots=function(){var e=this;if(e.opt.dots){if("string"==typeof e.opt.dots?e.dots=document.querySelector(e.opt.dots):e.dots=e.opt.dots,e.dots){e.dots.innerHTML="",e.dots.classList.add("glider-dots");for(var t=0;t<Math.ceil(e.slides.length/e.opt.slidesToShow);++t){var o=document.createElement("button");o.dataset.index=t,o.setAttribute("aria-label","Page "+(t+1)),o.className="glider-dot "+(t?"":"active"),e.event(o,"add",{click:e.scrollItem.bind(e,t,!0)}),e.dots.appendChild(o)}}}else e.dots&&(e.dots.innerHTML="")},t.bindArrows=function(){var o=this;o.opt.arrows?["prev","next"].forEach(function(e){var t=o.opt.arrows[e];t&&("string"==typeof t&&(t=document.querySelector(t)),t._func=t._func||o.scrollItem.bind(o,e),o.event(t,"remove",{click:t._func}),o.event(t,"add",{click:t._func}),o.arrows[e]=t)}):Object.keys(o.arrows).forEach(function(e){var t=o.arrows[e];o.event(t,"remove",{click:t._func})})},t.updateControls=function(e){var d=this;e&&!d.opt.scrollPropagate&&e.stopPropagation();var t=d.containerWidth>=d.trackWidth;d.opt.rewind||(d.arrows.prev&&d.arrows.prev.classList.toggle("disabled",d.ele.scrollLeft<=0||t),d.arrows.next&&d.arrows.next.classList.toggle("disabled",Math.ceil(d.ele.scrollLeft+d.containerWidth)>=Math.floor(d.trackWidth)||t)),d.slide=Math.round(d.ele.scrollLeft/d.itemWidth),d.page=Math.round(d.ele.scrollLeft/d.containerWidth);var c=d.slide+Math.floor(Math.floor(d.opt.slidesToShow)/2),h=Math.floor(d.opt.slidesToShow)%2?0:c+1;1===Math.floor(d.opt.slidesToShow)&&(h=0),d.ele.scrollLeft+d.containerWidth>=Math.floor(d.trackWidth)&&(d.page=d.dots?d.dots.children.length-1:0),[].forEach.call(d.slides,function(e,t){var o=e.classList,i=o.contains("visible"),r=d.ele.scrollLeft,s=d.ele.scrollLeft+d.containerWidth,l=d.itemWidth*t,n=l+d.itemWidth;[].forEach.call(o,function(e){/^left|right/.test(e)&&o.remove(e)}),o.toggle("active",d.slide===t),c===t||h&&h===t?o.add("center"):(o.remove("center"),o.add([t<c?"left":"right",Math.abs(t-(t<c?c:h||c))].join("-")));var a=Math.ceil(l)>=r&&Math.floor(n)<=s;o.toggle("visible",a),a!==i&&d.emit("slide-"+(a?"visible":"hidden"),{slide:t})}),d.dots&&[].forEach.call(d.dots.children,function(e,t){e.classList.toggle("active",d.page===t)}),e&&d.opt.scrollLock&&(clearTimeout(d.scrollLock),d.scrollLock=setTimeout(function(){clearTimeout(d.scrollLock),.02<Math.abs(d.ele.scrollLeft/d.itemWidth-d.slide)&&(d.mouseDown||d.trackWidth>d.containerWidth+d.ele.scrollLeft&&d.scrollItem(d.getCurrentSlide()))},d.opt.scrollLockDelay||250))},t.getCurrentSlide=function(){var e=this;return e.round(e.ele.scrollLeft/e.itemWidth)},t.scrollItem=function(e,t,o){o&&o.preventDefault();var i=this,r=e;if(++i.animate_id,!0===t)e*=i.containerWidth,e=Math.round(e/i.itemWidth)*i.itemWidth;else{if("string"==typeof e){var s="prev"===e;if(e=i.opt.slidesToScroll%1||i.opt.slidesToShow%1?i.getCurrentSlide():i.slide,s?e-=i.opt.slidesToScroll:e+=i.opt.slidesToScroll,i.opt.rewind){var l=i.ele.scrollLeft;e=s&&!l?i.slides.length:!s&&l+i.containerWidth>=Math.floor(i.trackWidth)?0:e}}e=Math.max(Math.min(e,i.slides.length),0),i.slide=e,e=i.itemWidth*e}return i.scrollTo(e,i.opt.duration*Math.abs(i.ele.scrollLeft-e),function(){i.updateControls(),i.emit("animated",{value:r,type:"string"==typeof r?"arrow":t?"dot":"slide"})}),!1},t.settingsBreakpoint=function(){var e=this,t=e._opt.responsive;if(t){t.sort(function(e,t){return t.breakpoint-e.breakpoint});for(var o=0;o<t.length;++o){var i=t[o];if(a.innerWidth>=i.breakpoint)return e.breakpoint!==i.breakpoint&&(e.opt=Object.assign({},e._opt,i.settings),e.breakpoint=i.breakpoint,!0)}}var r=0!==e.breakpoint;return e.opt=Object.assign({},e._opt),e.breakpoint=0,r},t.scrollTo=function(t,o,i){var r=this,s=(new Date).getTime(),l=r.animate_id,n=function(){var e=(new Date).getTime()-s;r.ele.scrollLeft=r.ele.scrollLeft+(t-r.ele.scrollLeft)*r.opt.easing(0,e,0,1,o),e<o&&l===r.animate_id?a.requestAnimationFrame(n):(r.ele.scrollLeft=t,i&&i.call(r))};a.requestAnimationFrame(n)},t.removeItem=function(e){var t=this;t.slides.length&&(t.track.removeChild(t.slides[e]),t.refresh(!0),t.emit("remove"))},t.addItem=function(e){this.track.appendChild(e),this.refresh(!0),this.emit("add")},t.handleMouse=function(e){var t=this;t.mouseDown&&(t.isDrag=!0,t.ele.scrollLeft+=(t.mouseDown-e.clientX)*(t.opt.dragVelocity||3.3),t.mouseDown=e.clientX)},t.round=function(e){var t=1/(this.opt.slidesToScroll%1||1);return Math.round(e*t)/t},t.refresh=function(e){this.init(!0,e)},t.setOption=function(t,e){var o=this;o.breakpoint&&!e?o._opt.responsive.forEach(function(e){e.breakpoint===o.breakpoint&&(e.settings=Object.assign({},e.settings,t))}):o._opt=Object.assign({},o._opt,t),o.breakpoint=0,o.settingsBreakpoint()},t.destroy=function(){function e(t){t.removeAttribute("style"),[].forEach.call(t.classList,function(e){/^glider/.test(e)&&t.classList.remove(e)})}var t=this,o=t.ele.cloneNode(!0);o.children[0].outerHTML=o.children[0].innerHTML,e(o),[].forEach.call(o.getElementsByTagName("*"),e),t.ele.parentNode.replaceChild(o,t.ele),t.event(a,"remove",{resize:t.resize}),t.emit("destroy")},t.emit=function(e,t){var o=new a.CustomEvent("glider-"+e,{bubbles:!this.opt.eventPropagate,detail:t});this.ele.dispatchEvent(o)},t.event=function(e,t,o){var i=e[t+"EventListener"].bind(e);Object.keys(o).forEach(function(e){i(e,o[e])})},e});
 
 new Glider(document.querySelector('.glider2'), {
@@ -36,8 +6,8 @@ new Glider(document.querySelector('.glider2'), {
   dragVelocity: 1.5,
 });
 
-function showArtist(){
-    fetch("./assets/js/data.json")
+function showArtists(){
+    fetch(`./assets/js/data.json`)
     .then(response => response.json())
     .then(function(data){
         return data.artists;
@@ -47,14 +17,17 @@ function showArtist(){
         artists.forEach(function(artist){
             const artistCards = document.createElement('div');
             artistCards.classList.add('artistCard');
-
+            artistCards.setAttribute('data-artist', artist.name);
+            artistCards.setAttribute('data-artistImg', artist.img);
+            let albums = artist.albums;
+            for(let i=0; i<albums.length; i++){
+                artistCards.setAttribute('data-albums', albums[i].album)
+            }
             artistCards.innerHTML = `
             <img src="${artist.img}" alt="">
             <h2>${artist.name}</h2>
         `
             container.appendChild(artistCards);
-
-            
         })
         new Glider(document.querySelector('.glider'), {
             slidesToShow: 3.5,
@@ -63,8 +36,38 @@ function showArtist(){
         });
     })
 }
-showArtist();
-  
+showArtists();
+
+
+// When clicking on artist
+let artistEl = document.querySelector(".artistCards")
+artistEl.addEventListener("click", (e) => {
+    if(e.target.tagName === "DIV"){
+        const artist = e.target.getAttribute('data-artist');
+        const artistImg = e.target.getAttribute('data-artistImg');
+        const artistAlbums = e.target.getAttribute('data-albums');
+        const artistSingles = e.target.getAttribute('data-singles');
+        getArtist(artist, artistImg, artistAlbums, artistSingles);
+    }
+});
+function getArtist(artist, artistImg, artistAlbums, artistSingles){
+    let main = document.querySelectorAll("main");
+    let popup = document.querySelector(".popup");
+    let popupName = document.querySelector(".popup h1");
+    let popupImg = document.querySelector(".popup img");
+    let popupAlbums = document.querySelector("#albumsUl")
+    let popupSingles = document.querySelector("#singlesUl")
+
+    popupName.innerHTML = artist;
+    popupImg.src = artistImg;
+    popupAlbums.innerHTML = `<li>${artistAlbums}</li>`;
+    popupSingles.innerHTML = `<li>${artistSingles}</li>`;
+    main.forEach(function(element){
+        element.style.display = "none";
+    })
+    popup.style.display = "block";
+}
+
 
 
 function featuredSong(){
@@ -94,3 +97,10 @@ function featuredSong(){
     })
 }
 featuredSong();
+
+async function topAlbums(){
+    const resp = await fetch(`./assets/js/data.json`);
+    const data = await resp.json();
+    //console.log(data);
+}
+topAlbums();
--- assets/js/search.js
+++ assets/js/search.js
@@ -9,9 +9,21 @@ const apiUrl = "https://api.lyrics.ovh";
 async function searchSongs(term){
     const resp = await fetch(`${apiUrl}/suggest/${term}`);
     const data = await resp.json();
-
-    showData(data);
+    console.log(`Search suggestions for ${term}`);
+    console.log(data);
+    showData(data)
 }
+
+// When submitting search term
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const searchTerm = search.value.trim();
+    if(!searchTerm){
+        alert("Please type a search term")
+    } else {
+        searchSongs(searchTerm)
+    }
+});
 
 //show song and artist in DOM
 function showData(data){
@@ -20,7 +32,7 @@ function showData(data){
         output += `
         <li>
             <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-            <button class="lyricBtn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+            <button class="lyricBtn" data-artist="${song.artist.name}" data-songtitle="${song.title}" data-audio="${song.preview}">Get Lyrics</button>
         </li>
         `
     })
@@ -35,10 +47,9 @@ function showData(data){
         ${data.prev ? `<button class="pageBtn" onclick="getMoreSongs('${data.prev}')">Prev</button>` : ''}
         ${data.next ? `<button class="pageBtn" onclick="getMoreSongs('${data.next}')">Next</button>` : ''}
         `;
-    } else {
-        more.innerHTML = '';
     }
 }
+
 
 // Get prev and next songs
 async function getMoreSongs(url) {
@@ -48,37 +59,33 @@ async function getMoreSongs(url) {
     showData(data);
 }
 
-// When submitting search term
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    const searchTerm = search.value.trim();
-    if(!searchTerm){
-        alert("Please type a search term")
-    } else {
-        searchSongs(searchTerm)
-    }
-});
-
 //'Get Lyrics' button click
-result.addEventListener("click", e => {
-    if(e.target.tagName === "BUTTON"){
+result.addEventListener("click", (e) => {
+    if(e.target.className === "lyricBtn"){
         const artist = e.target.getAttribute('data-artist');
         const songTitle = e.target.getAttribute('data-songtitle');
+        const preview = e.target.getAttribute('data-audio');
 
-        getLyrics(artist, songTitle);
+        getLyrics(artist, songTitle, preview);
     }
 })
 
 // Get lyrics for song
-async function getLyrics(artist, songTitle){
+async function getLyrics(artist, songTitle, preview){
     const resp = await fetch(`${apiUrl}/v1/${artist}/${songTitle}`);
     const data = await resp.json();
 
     const lyrics = data.lyrics.replace(/(\r\r|\n)/g, '<br>');
-
-    result.innerHTML = `
-    <h2><strong>${artist}</strong> - ${songTitle}</h2>
-    <span>${lyrics}</span>`;
+    if(lyrics == ""){
+        result.innerHTML = `<h2>No lyrics found</h2>`
+    }else{
+        result.innerHTML = `
+        <audio controls>
+            <source src="${preview}" type="audio/mp3">
+        </audio>
+        <h2><strong>${artist}</strong> - ${songTitle}</h2>
+        <span>${lyrics}</span>`;
+    }
 
     more.innerHTML = '';
 }
